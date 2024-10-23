b40df29db7b642a1953061878c2517d2956469c0
AshuChoudharystd <146466303+AshuChoudharystd@users.noreply.github.com>
2024-10-23 21:33:17 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/Logo.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon style" style = {{paddingTop:"20px"}}>
+              <a href="https://www.linkedin.com/in/mahendra-choudhary-7912b52a4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="https://github.com/AshuChoudharystd/AshuChoudharystd">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="https://www.instagram.com/mahendra_jat17?igsh=ZHU1M24xa3EzMzNw">
+                <img src={navIcon3} alt="" />
+              </a>
+            </div>
+            <p>Copyright 2024. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
